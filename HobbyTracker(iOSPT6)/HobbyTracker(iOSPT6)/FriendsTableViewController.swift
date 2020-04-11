2839e714cb45dc46b97a16490fd24e173b75c0b1
Your Name Here <youremailhere>
2020-04-10 20:56:52 -0400

    Pushing new updates to files

---
+++ HobbyTracker(iOSPT6)/HobbyTracker(iOSPT6)/FriendsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FriendsTableViewController.swift
+//  HobbyTracker(iOSPT6)
+//
+//  Created by Sammy Alvarado on 4/9/20.
+//  Copyright © 2020 Sammy Alvarado. All rights reserved.
+//
+
+import UIKit
+
+class FriendsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
